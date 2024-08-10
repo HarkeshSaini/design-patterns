b21d0607656894bb08555384338d225f34369ea0
Harkesh Saini <harkesh@sbsind.in>
2024-08-10 10:53:43 +0530

    initial commit

---
+++ src/test/java/Spring/boot/design/pattrens/design_patterns/DesignPatternsApplicationTests.java
@@ -0,0 +1,13 @@
+package Spring.boot.design.pattrens.design_patterns;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesignPatternsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
